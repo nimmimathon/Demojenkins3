251be64b15c25631e584a8cfcd5316f8035832f6
nimmimathon <75145201+nimmimathon@users.noreply.github.com>
2021-04-21 11:53:07 +0530

    Add files via upload

---
+++ contdemo.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module BcContainerHelper
+$additionalParameters = '-v .\tmp:C:\Run\tmp'
+$containerName = 'testnavs331'
+$password = 'P@ssw0rd'
+$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+$credential = New-Object pscredential 'admin', $securePassword
+$auth = 'UserPassword'
+$artifactUrl = Get-NavArtifactUrl -nav 2018 -cu 18 -country de
+$licenseFile = '.\Demojenkins\DevLicense2.flf'
+New-BcContainer `
+    -accept_eula `
+    -containerName $containerName `
+    -credential $credential `
+    -auth $auth `
+    -artifactUrl $artifactUrl `
+    -imageName 'navisionimg1' `
+    -licenseFile $licenseFile `
+    -memoryLimit 4G `
+    -additionalParameters $additionalParameters `
+    -updateHosts
+
+Import-DeltasToNavContainer -containerName testnavs331 -deltaFolder '.\tmp'
