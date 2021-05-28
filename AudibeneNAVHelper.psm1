16efec011d5c7cd789cc41090c73561a0c770813
nimmimathon <75145201+nimmimathon@users.noreply.github.com>
2021-05-28 10:55:25 +0200

    Update AudibeneNAVHelper.psm1

--- AudibeneNAVHelper.psm1
+++ AudibeneNAVHelper.psm1
@@ -2,5 +2,5 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
 $ErrorActionPreference = "stop"
 
 # NAVAPP STORAGE
-. (Join-Path $PSScriptRoot "private/Get-AzureStorage.ps1")
-. (Join-Path $PSScriptRoot "private/Get-AzureStorage1.ps1")
+. (Join-Path $PSScriptRoot "tmp/Get-AzureStorage.ps1")
+. (Join-Path $PSScriptRoot "tmp/Get-AzureStorage1.ps1")
